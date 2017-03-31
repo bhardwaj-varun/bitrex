eaefdca3f24c840ca50cdb06c6e706c0922225d8
varun bhardwaj <varunbhardwaj.16dec@gmail.com>
2017-04-01 02:18:25 +0530

    added DatabaseInfo

---
+++ database/DatabaseInfo.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author root
+ */
+interface DatabaseInfo {
+    //put your code here
+    const db_hostname='localhost';
+    const db_database='';
+    const db_username='';
+    const db_password='';
+}
