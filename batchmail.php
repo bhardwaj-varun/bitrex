76bc77a249a3339b759be9c8a6246ab1fb40d360
varun <varunbhardwaj.16dec@gmail.com>
2017-04-06 09:30:29 +0530

    batchmail

--- batchmail.php
+++ batchmail.php
@@ -1,34 +1,56 @@
 <?php
 
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/4/17
+ * Time: 9:26 AM
+ */
 require_once 'database/Database.php';
 require_once 'Mail.php';
- $db=new Database();
 
- function sendMail($email,$regtype,$isc){
-    $mail=new Mail();
-    if($isc==1 && $regtype==1){ //student of iSC and tshirt not purchased
-        $mail->setPayment(0,0);
-    }
-    else if($isc==1 && $regtype==2 ){
-        $mail->setPayment(1,300);
-    }
-    else if($isc=0 && $regtype==1 ){
-        $mail->setPayment(1,200);
-    }
-    else if($isc==0 && $regtype==2 ){
-        $mail->setPayment(1,500);
-    }
-    $mail->setMessage();
-    $mail->setTo($email);
-    $mail->setSubject("BITREX'17 | REGISTRATION");
-    $mail->sendMail();
+class batchmail
+{
 
- }
+    private $rows;
 
- $sql="select email,regtype,isc from info";
- $result=$db->query($sql);
- $rows=$db->getMultipleResultantRows();
- print_r($rows);
- //for($i=0;$i<count($rows);$i++)
- //sendmail('varunbhardwaj.16dec@gmail.com',1,1);
 
+    function sendMail($email,$regtype,$isc){
+        $mail=new Mail();
+        if($isc==1 && $regtype==1){ //student of iSC and t-shirt not purchased
+            $mail->setPayment(0,0);
+        }
+        else if($isc==1 && $regtype==2 ){
+            $mail->setPayment(1,300);
+        }
+        else if($isc=0 && $regtype==1 ){
+            $mail->setPayment(1,200);
+        }
+        else if($isc==0 && $regtype==2 ){
+            $mail->setPayment(1,500);
+        }
+        $mail->setMessage();
+        $mail->setTo($email);
+        $mail->setSubject("BITREX'17 | REGISTRATION");
+        $mail->sendMail();
+    }
+    public function getEmails(){
+        $db=new Database();
+        $db->query = "call selectmail()";
+        $db->stmt = $db->prepare($db->query);
+        $db->stmt->execute();
+        $this->rows= $db->getMultipleResultantRows();
+    }
+    public function emailer(){
+        for($i=0;$i<count($this->rows);$i++) {
+            print_r($this->rows);
+            //sendmail('varunbhardwaj.16dec@gmail.com',1,1);
+        }
+    }
+
+
+
+}
+$obj=new batchmail();
+$obj->getEmails();
+$obj->emailer();
