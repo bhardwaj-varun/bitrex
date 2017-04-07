d74e1aee031c441940a5ce514cac1cf83727e525
varun <varunbhardwaj.16dec@gmail.com>
2017-04-07 23:43:15 +0530

    info page

--- Info.php
+++ Info.php
@@ -6,8 +6,46 @@
  * Date: 7/4/17
  * Time: 12:43 AM
  */
-require_once ''
+require_once 'database/Database.php';
 class Info
 {
+      private  $infoResult,$tshirtResult,$email;
+
+
+    public function getParticipantsInfo() {
+        $db=new Database();
+        $db->query = "call get_registration_info(?)";
+        $db->stmt = $db->prepare($db->query);
+        $db->stmt->bind_param('s',$this->email);
+        $db->stmt->execute();
+        $this->infoResult =$db->getResultantRow();
+
+    }
+    public function setEmail($email){
+        $this->email=$email;
+    }
+    public function fetchData(){
+        $this->getParticipantsInfo();
+        if($this->infoResult['regtype']==2){
+            $this->getTshirtInfo();
+            $this->infoResult=array_merge($this->infoResult,$this->tshirtResult);
+        }
+    }
+    public function getTshirtInfo(){
+        $db=new Database();
+        $db->query = "call get_tshirt_info(?)";
+        $db->stmt = $db->prepare($db->query);
+        $db->stmt->bind_param('s',$this->email);
+        $db->stmt->execute();
+        $this->tshirtResult =$db->getResultantRow();
+    }
+    public function getJSONResult(){
+        echo json_encode(array("msg"=> $this->infoResult));
+    }
 
 }
+$info=new Info();
+$json = json_decode(file_get_contents("php://input"));
+$info->setEmail($json->email);
+$info->fetchData();
+$info->getJSONResult();
--- js/info.js
+++ js/info.js
@@ -2,9 +2,40 @@ $(document).ready(function () {
 
     $(document).on('click','#submit-info',function(e){
             e.preventDefault();
-            alert('ehllo');
-            for(var i=0;i<3;i++){
-                $('#detail-info').append('<p class="card-text  text-center">T-Shirt : <span>Text on Black</span></p>')
-            }
+            var info={email:$('#email-info').val()}
+            info=JSON.stringify(info);
+            getInfo(info);
     });
 });
+var getInfo = function (info) {
+    var div='';
+    var jsondata='';
+    $.ajax({
+        url: "Info.php",
+        type: "post",
+        data: info,
+        async: true,
+        success: function (response) {
+            console.log(response);
+            jsondata=$.parseJSON(response);
+            if(jsondata.msg.registered==1){
+                $('#name').html(jsondata.msg.name);
+                $('#isc').html('From I.Sc: '+jsondata.msg.isc);
+                $('#college').html(jsondata.msg.college);
+                $('#university').html(jsondata.msg.university);
+                if(jsondata.msg.regtype==2) {
+                    $('#style').html('T-Shirt Style: '+jsondata.msg.style);
+                    $('#size').html('T-Shirt Size: '+jsondata.msg.size);
+                }
+            }else $('#name').html('Not Registered');
+        },
+        error: function (response, status, errorThrown) {
+            console.log(response + status);
+            alert(response); //result from server if error occured
+            alert(errorThrown);  //error code
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+};
